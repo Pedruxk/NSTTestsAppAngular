80bc14018b152feb2867095f9bfc1243180dc6f3
Pedro Antonio Viana Martín <pedro.viana@blackbaud.me>
2022-07-13 11:59:44 -0500

    final changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,15 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { DemoComponent } from './demo/demo.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    DemoComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, DemoComponent, HomeComponent],
+  imports: [BrowserModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
